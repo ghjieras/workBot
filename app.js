f8269fcda0d7be6fa70ba0fda166e2d59e8f6efa
呂念浯wade <lu_wade@wt27.net>
2022-08-02 10:30:26 +0800

    create procfile

--- app.js
+++ app.js
@@ -72,7 +72,18 @@ function readData (Msg) {
   return result == undefined ? " " : result[0]
 }
 
+const app = express();
+const linebotParser = bot.parser();
+app.get("/", function (req, res) { 
+    res.send("Hello LineBot");
+});
+app.post('/', linebotParser);
 
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
 
 
 // function replyMsg(Msg) {
